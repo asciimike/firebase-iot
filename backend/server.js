34686ad28f146ed854ab736241fc907db5d8fe6a
Michael McDonald <mcdonald@firebase.com>
2014-10-05 20:46:48 -0400

    Added in bonescript

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// var bonescript = require('bonescript');
+var bonescript = require('bonescript');
 var Firebase = require('firebase');
 // var greenBean = require("green-bean");
 
@@ -11,48 +11,49 @@ function main(){
 	var lightRef = ref.child('led');
 	var tempRef = ref.child('temperature');
 	var fridgeRef = ref.child('appliances/refrigerator');
-	var rangeRef = ref.child('appliances/range')
+	var rangeRef = ref.child('appliances/range');
 
 	// LED Firebase Callback
 	lightRef.on('value', function(snapshot){
 		// Update the LED value 
 		var state = snapshot.val();
 		console.log('Updating the state of the LED to ' + state);
-		// bonescript.digitalWrite(led, state);
+		bonescript.pinMode(led, b.OUTPUT);
+		bonescript.digitalWrite(led, state);
 	});
 
 	// Fridge Greenbean Callback
-	greenBean.connect("refrigerator", function (refrigerator) {
-		// Subscribe to the temp alert
-		refrigerator.temperatureAlert.subscribe(function (value) {
-			fridgeRef.child('temperatureAlert').set(value);
-			console.log("temperature alert:", value);
-		});
+	// greenBean.connect("refrigerator", function (refrigerator) {
+	// 	// Subscribe to the temp alert
+	// 	refrigerator.temperatureAlert.subscribe(function (value) {
+	// 		fridgeRef.child('temperatureAlert').set(value);
+	// 		console.log("temperature alert:", value);
+	// 	});
 
-		// Subscribe to the door state alert
-		refrigerator.doorState.subscribe(function (value) {
-			fridgeRef.child('doorState').set(value);
-			console.log("door state changed:", value);
-		});
-	});
+	// 	// Subscribe to the door state alert
+	// 	refrigerator.doorState.subscribe(function (value) {
+	// 		fridgeRef.child('doorState').set(value);
+	// 		console.log("door state changed:", value);
+	// 	});
+	// });
 
 	// Range Greenbean Callback
-	greenBean.connect("range", function(range) {
-		range.lightBar.read(function(value) {
-			console.log("light bar is:", value);
-		});
+	// greenBean.connect("range", function(range) {
+	// 	range.lightBar.read(function(value) {
+	// 		console.log("light bar is:", value);
+	// 	});
 
-		range.lightBar.subscribe(function(value) {
-			console.log("light bar changed:", value);
-		});
-	});
+	// 	range.lightBar.subscribe(function(value) {
+	// 		console.log("light bar changed:", value);
+	// 	});
+	// });
 
 	//Range Firebase Callback
-	rangeRef.on('value', function(snapshot){
-		greenBean.connect("range", function(range) {
-			range.lightBar.write(snapshot.val());
-		});
-	});
+	// rangeRef.on('value', function(snapshot){
+	// 	greenBean.connect("range", function(range) {
+	// 		range.lightBar.write(snapshot.val());
+	// 	});
+	// });
 }
 
 main();
